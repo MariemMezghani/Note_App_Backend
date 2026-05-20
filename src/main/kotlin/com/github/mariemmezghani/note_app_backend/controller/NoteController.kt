83c8a12f542697a9c022ec9a4adfd0f2f6c583e1
MariemMezghani <mariem.mezghani@hotmail.com>
2026-05-20 12:25:46 +0200

    add HTTP methods in NoteController

--- src/main/kotlin/com/github/mariemmezghani/note_app_backend/controller/NoteController.kt
+++ src/main/kotlin/com/github/mariemmezghani/note_app_backend/controller/NoteController.kt
@@ -1,17 +1,20 @@
 package com.github.mariemmezghani.note_app_backend.controller
 
 import com.github.mariemmezghani.note_app_backend.database.model.Note
+import com.github.mariemmezghani.note_app_backend.database.repository.NoteRepository
 import org.bson.types.ObjectId
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.time.Instant
 
-// POST http:localhost:8080/notes
-
+// POST http://8080:localhost/notes
+// POST http://8080:localhost/notes?ownerId=123
 @RestController
 @RequestMapping("/notes")
-class NoteController {
+class NoteController(val repository: NoteRepository) {
 
     data class NoteRequest(
         val id: String,
@@ -39,12 +42,24 @@ class NoteController {
             timeStamp = Instant.now(),
             owner = ObjectId(body.ownerId)
         )
+        repository.save(note)
+        return note.toNoteResponse()
+    }
+
+    @GetMapping
+    fun getNotesByOwnerId(@RequestParam(required = true) ownerId: String): List<NoteResponse> {
+        return repository.findByOwner(ObjectId(ownerId)).map {
+            it.toNoteResponse()
+        }
+    }
+
+    private fun Note.toNoteResponse(): NoteResponse {
         return NoteResponse(
-            id = note.id.toHexString(),
-            title = note.title,
-            content = note.content,
-            color = note.color,
-            timeStamp = note.timeStamp,
+            id = id.toHexString(),
+            title = title,
+            content = content,
+            color = color,
+            timeStamp = timeStamp
         )
     }
 }
